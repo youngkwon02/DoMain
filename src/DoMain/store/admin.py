7c7414004d1c1c6e9ed8bc00bcc04814ff608599
Seojisoo20191941 <amy0480@naver.com>
2021-08-13 21:53:32 +0900

    feat:fix profile-image change#2

--- src/DoMain/store/admin.py
+++ src/DoMain/store/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from store.models import StoreWidget, Comment, Reply
+from store.models import StoreWidget, Comment, Reply,Profile
 
 admin.site.register(StoreWidget)
 admin.site.register(Comment)
 admin.site.register(Reply)
+admin.site.register(Profile)
